b3cbe3805d78ff7eeb2a5c38a35c365a94ed44c0
Leop0ld <l3opold7@gmail.com>
2016-10-24 13:14:00 +0900

    unittest 사용

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,5 @@
+import unittest
+
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
@@ -5,9 +7,20 @@ caps = DesiredCapabilities.FIREFOX
 caps["marionette"] = True
 caps["binary"] = "/Applications/Firefox.app/Contents/MacOS/firefox-bin"
 
-browser = webdriver.Firefox(capabilities=caps)
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
 
-browser.quit()
+    def setUp(self):
+        self.browser = webdriver.Firefox(capabilities=caps)
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
