794e60b1cf2ba60c30be378a90805dce5e8333bc
Leop0ld <l3opold7@gmail.com>
2016-11-23 16:21:11 +0900

    Add functional_test and Remove useless file in Git & Add these at .gitignore

---
+++ functional_tests.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+caps = DesiredCapabilities.FIREFOX
+caps["marionette"] = True
+caps["binary"] = "/Applications/Firefox.app/Contents/MacOS/firefox-bin"
+
+browser = webdriver.Firefox(capabilities=caps)
+browser.get('http://localhost:8000')
+
+assert 'To-Do' in browser.title
